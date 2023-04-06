59922d2b70f77fa688f669f02aa35d1bbd76cf40
Tu Anh <ntuanh490@gmail.com>
2023-04-06 17:48:29 +0700

    Styling pages, reformatting and refactoring

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.set('strictQuery', true);
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 }
 
 const db = mongoose.connection;
--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,7 @@ mongoose.set('strictQuery', true);
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 }
 
 const db = mongoose.connection;
@@ -25,9 +25,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/2184453',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit in modi laudantium accusantium sed deserunt atque cum? Asperiores vitae obcaecati architecto dicta ducimus totam, minus, doloribus, fugiat quidem voluptatem error.',
+            price
         })
         await camp.save();
     }
