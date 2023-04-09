0079680bc6f6b979454429f1207a37abc5345c17
Tu Anh <ntuanh490@gmail.com>
2023-04-09 16:45:38 +0700

    Add review deletion and update review ui

--- models/review.js
+++ models/review.js
@@ -1,9 +1,9 @@
-const mongoose = reuqire('mongoose');
-const Schema = mongoose.Schmea;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     body: String,
     rating: Number
 });
 
-module.exports = mongoose.model("Review", reviewSchema)
+module.exports = mongoose.model("Review", reviewSchema);
