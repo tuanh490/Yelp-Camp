137d4c2a76cd1aefc9e5ab0cb1bb5a3a19f35266
Tu Anh <ntuanh490@gmail.com>
2023-04-13 18:38:43 +0700

    Add register form, login route and form

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const LocalStrategy = require('passport-local');
 
 const ExpressError = require('./utils/ExpressError');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 const User = require('./models/user');
 
@@ -73,8 +74,9 @@ app.get('/fakeUser', async (req, res) => {
     res.send(newUser)
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
